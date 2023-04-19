7f78ed409cd8510a8da01d58120f0492b27fa7f3
KHJH988 <98236360+KHJH988@users.noreply.github.com>
2023-04-19 11:11:36 +0100

    screen and save load tests

---
+++ PiazzaPanic1/PiazzaPanic1-main/core/src/com/neves6/piazzapanic/ScreenTests.java
@@ -0,0 +1,27 @@
+package com.neves6.piazzapanic;
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeUnit;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import org.junit.Test;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import org.junit.runner.RunWith;
+
+/**
+ * changes current screen, checks the transitions between screens is smooth
+ */
+@RunWith(GdxTestRunner.class)
+public class ScreenTests {
+
+
+    TiledMap map;
+
+    @Test
+    public void example(){
+
+    }
+}
